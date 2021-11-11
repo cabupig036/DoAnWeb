2140a6b391fe5ca78f114cdcb4bfe9269a2c102a
danh2411 <72093554+danh2411@users.noreply.github.com>
2021-11-12 00:02:41 +0700

    master page(home,contact,pro_detail)

--- Pcoint/pcoint/Contact.php
+++ Pcoint/pcoint/Contact.php
@@ -36,85 +36,12 @@
 
 <body class="main-layout">
    <!-- loader  -->
-   <div class="loader_bg">
-      <div class="loader"><img src="images/loading.gif" alt="#" /></div>
-   </div>
-   <!-- end loader -->
-   <!-- header -->
-   <header>
-         <!-- header inner -->
-         <div  class="head_top">
-            <div class="header">
-               <div class="container">
-                  <div class="row">
-                     <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
-                        <div class="full">
-                           <div class="center-desk">
-                              <div class="logo">
-                                 <a href="index.html"><img src="images/logo.png" alt="#" /></a>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
-                        <nav class="navigation navbar navbar-expand-md navbar-dark ">
-                           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-                           <span class="navbar-toggler-icon"></span>
-                           </button>
-                           <div class="collapse navbar-collapse" id="navbarsExample04">
-                              <ul class="navbar-nav mr-auto">
-                                 <li class="nav-item">
-                                    <a class="nav-link" href="./Homepage.php"> HomePage </a>
-                                 </li>
-                                 <li class="nav-item">
-                                    <a class="nav-link" href="./Product.php" style="color:gray;">Product</a>
-                                 </li>
-                                 <li class="nav-item">
-                                    <a class="nav-link" href="./About.php" style="color:gray;">About</a>
-                                 </li>
-                                 <li class="nav-item">
-                                    <a class="nav-link" href="./Contact.php" style="color:gray;">Contact us</a>
-                                 </li>
-                              </ul>
-                              <div class="sign_btn"><a href="/login-form-v1/Login_v1/Login.php">Sign in</a></div>
-                           </div>
-                        </nav>
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <!-- end header inner -->
-            <!-- end header -->
-            <!-- banner -->
-            <section class="banner_main">
-               <div class="container-fluid">
-                  <div class="row d_flex">
-                     <div class="col-md-5">
-                        <div class="text-bg">
-                           <h1>Computer and <br>laptop shop</h1>
-                           <strong>Free Multipurpose Responsive</strong>
-                           <span>Landing Page 2019</span>
-                           <a href="#">Buy Now</a>
-                        </div>
-                     </div>
-                     <div class="col-md-7 padding_right1">
-                        <div class="text-img">
-                           <figure><img src="images/top_img.png" alt="#"/></figure>
-                           <h3>01</h3>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </section>
-         </div>
-      </header>
-   <!-- end banner -->
-   <!-- about -->
    
-   <!-- about -->
-   <!-- best -->
-
-   <!-- end best -->
+         <?php
+      include 'header_banner.php';
+      ?>
+   <!-- end banner -->
+   
    <!-- request -->
    <div id="contact" class="request">
       <div class="container">
@@ -379,36 +306,9 @@
    <!-- end testimonial -->
    <!--  footer -->
    <footer>
-      <div class="footer">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-6">
-                  <div class="cont">
-                     <h3> <strong class="multi"> Free Multipurpose</strong><br>
-                        Responsive Landing Page 2019
-                     </h3>
-                  </div>
-               </div>
-               <div class="col-md-6">
-                  <div class="cont_call">
-                     <h3> <strong class="multi"> Call Now</strong><br>
-                        (+1) 12345667890
-                     </h3>
-                  </div>
-               </div>
-            </div>
-         </div>
-         <div class="copyright">
-            <div class="container">
-               <div class="row">
-                  <div class="col-md-12">
-                     <p>© 2019 All Rights Reserved. Design by <a href="https://html.design/"> Free Html Templates</a></p>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-   </footer>
+      <?php
+  include 'footer.php';
+?>
    <!-- end footer -->
    <!-- Javascript files-->
    <script src="js/jquery.min.js"></script>
